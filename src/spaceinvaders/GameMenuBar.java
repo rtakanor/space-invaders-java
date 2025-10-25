8eb6c92569d9bb0c80c3f783d7c43d0d8800bf09
Rodrigo Takano Romero <hig29@txstate.edu>
2025-10-25 13:48:20 -0600

    Implemented empty MenuBar

--- src/spaceinvaders/GameMenuBar.java
+++ src/spaceinvaders/GameMenuBar.java
@@ -1,3 +1,19 @@
-public class GameMenuBar {
+package spaceinvaders;
+
+import javax.swing.*;
+
+public class GameMenuBar extends JMenuBar {
+
+    public GameMenuBar(){
+        JMenu shooterMenu = new JMenu("Shooter Image");
+        JMenu invaderMenu = new JMenu("Invader Image");
+        JMenu bulletMenu = new JMenu("Bullet Type");
+        JMenu musicMenu = new JMenu("Music");
+
+        add(shooterMenu);
+        add(invaderMenu);
+        add(bulletMenu);
+        add(musicMenu);
+    }
     
 }
