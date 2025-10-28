a0da06e07c09da755599d30c51f0f72eecd1ae0e
Rodrigo Takano Romero <hig29@txstate.edu>
2025-10-28 14:29:47 -0600

    Added menuinvader object to gamemenubar

--- src/spaceinvaders/ui/GameMenuBar.java
+++ src/spaceinvaders/ui/GameMenuBar.java
@@ -1,19 +1,20 @@
 package spaceinvaders.ui;
-import spaceinvaders.utilities.ImageSelection;
-
 import javax.swing.*;
+import spaceinvaders.utilities.ImageSelection;
 
 public class GameMenuBar extends JMenuBar {
 
     public GameMenuBar(ImageSelection imageSelection, SpaceInvadersUI game){
 
         ShooterImageMenu menuShooter= new ShooterImageMenu(imageSelection, game);
+        InvaderImageMenu menuInvader= new InvaderImageMenu(imageSelection, game);
 
         JMenu invaderMenu = new JMenu("Invader Image");
         JMenu bulletMenu = new JMenu("Bullet Type");
         JMenu musicMenu = new JMenu("Music");
 
         add(menuShooter);
+        add(menuInvader);
         add(invaderMenu);
         add(bulletMenu);
         add(musicMenu);
